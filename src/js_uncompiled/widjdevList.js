7d9af2b6fd00306ad7f64047b6b1e5524ecfd1da
n <n@n-desktop.(none)>
2013-08-02 04:42:17 +0300

    first list widget extended skeleton code compiles and runs

--- src/js_uncompiled/widjdevList.js
+++ src/js_uncompiled/widjdevList.js
@@ -33,12 +33,43 @@ widjdev.list.Row.prototype.makeRowDom = function() {
   // Create DOM structure to represent the note.
   this.headerElement = goog.dom.createDom('div', {'style': 'background-color:#EEE'}, this.title);
   this.contentElement = goog.dom.createDom('div', null, this.content);
-  var newRow = goog.dom.createDom('div', null, this.headerElement, this.contentElement);
+  
+   // Create the editor text area and save button.
+  this.editorElement = goog.dom.createDom('textarea');
+  var saveBtn = goog.dom.createDom('input',{'type': 'button', 'value': 'Save'});
+  this.editorContainer = goog.dom.createDom('div', {'style': 'display:none;'},
+  this.editorElement, saveBtn);
+
+	//header in content containerwrap
+  this.contentContainer = goog.dom.createDom('div', null, this.contentElement, this.editorContainer);
+  
+    // Wrap the editor and the content div in a single parent so they can be toggled in unison.
+  var newRow = goog.dom.createDom('div', null, this.headerElement, this.contentContainer);
 
   // Add the note's DOM structure to the document.
   goog.dom.appendChild(this.parent, newRow );
-  return new goog.ui.Zippy(this.headerElement, this.contentElement);
+  
+   // Attach the event handler that opens the editor.
+  goog.events.listen(this.contentElement, goog.events.EventType.CLICK, this.openEditor);
+  
+  return new goog.ui.Zippy(this.headerElement, this.contentContainer);
 };
+
+
+
+
+ widjdev.list.Row.prototype.openEditor = function(e) {
+  var elt = e.target;
+  var content = goog.dom.getTextContent(elt);
+  var editorContainer = goog.dom.getNextElementSibling(elt);
+  var editor = goog.dom.getFirstElementChild(editorContainer);
+
+// Put the row contents into the editor field.
+  editor.innerHTML = content;
+  elt.style.display = 'none';
+  editorContainer.style.display = 'inline';
+};
+
 
 
 // export the main namespace and public functions
