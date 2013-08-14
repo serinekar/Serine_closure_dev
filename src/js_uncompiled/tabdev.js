d89af45915a15522dda596d202042685d9c581b5
n <n@n-desktop.(none)>
2013-08-14 18:25:36 +0300

    refactor googtol bar to tabs forlist widj ok no css

--- src/js_uncompiled/tabdev.js
+++ src/js_uncompiled/tabdev.js
@@ -1,4 +1,8 @@
 goog.provide('widjdev.tabdev');
+
+goog.provide('widjdev.tabdev.tabRow');
+
+goog.require('widjdev.Component'); // this is a custom dependency
 goog.require('goog.dom');
 goog.require('goog.ui.Zippy');
 goog.require('goog.ui.CustomButton'); // this is a standard dependency
@@ -12,12 +16,12 @@ goog.require('goog.ui.RoundedTabRenderer');
 goog.require('goog.ui.Tab');
 goog.require('goog.ui.TabBar');
 
-
+var rowList;
 
 widjdev.tabdev.setTabs= function(container) {
 
  		//container for the rows, will attach to first tab
- 		var rowList = goog.dom.createDom('div', { 'class': 'snipList' });
+ 	 rowList = goog.dom.createDom('div', { 'class': 'snipList' });
 
     var topTab = new goog.ui.TabBar();
     topTab.decorate(goog.dom.getElement(container));
@@ -25,7 +29,17 @@ widjdev.tabdev.setTabs= function(container) {
     
     // set the initial view
     goog.dom.setTextContent(rowList,  'You have added the rows');
-    contentElement.appendChild(rowList);   
+    contentElement.appendChild(rowList); 
+    
+     // called when we make the rows below
+ 		var makeRowData = function(summary, description, priority) {  
+ 
+ 		return { 'summary': summary, 'description': description }; };
+ 		
+    widjdev.tabdev.makeRows('active', [ makeRowData("Header first row", "header second row"),  makeRowData("Header first row", "header second row"),
+              makeRowData("Header first row", "header second row")], rowList );
+    
+      
         // Handle SELECT events dispatched by tabs.
     goog.events.listen(topTab, goog.ui.Component.EventType.SELECT,
             function(e) {
@@ -46,9 +60,90 @@ widjdev.tabdev.setTabs= function(container) {
             });
 
 }
+// a rowList object
+widjdev.tabdev.rowLists = {};
 
+// the row class  widjdev.tabdev.tabRow
+widjdev.tabdev.tabRow = function(data, container) {
 
+// these are the instance variables, they will needto become custom widgets
+// data is a javascript object
+    
+    this.summary = data.summary;
+    this.description = data.description;
+    // main root node in html for rows
+    this.parent = container;
+    
+}
+ // static(class) methods to open and close the editor
+widjdev.tabdev.tabRow.prototype.closeEditor = function() {
 
+  this.editorContainer.style.display = "none";
+};
+
+widjdev.tabdev.tabRow.prototype.openEditor = function(e) {
+
+  this.editorContainer.style.display = "inline";
+};
+
+// create instances of the row class  widjdev.list.Row
+widjdev.tabdev.makeRows = function(name, data, container) {
+
+        for (var i = 0; i < data.length; i++) {
+            var row = new widjdev.tabdev.tabRow(data[i], container);
+            row.makeDom();
+        }
+		// add row to rowList object
+    widjdev.tabdev.rowLists[name] = container;
+}
+
+widjdev.tabdev.tabRow.prototype.makeDom = function() {
+
+    this.summaryDiv = goog.dom.createDom('div', { 'class': 'summary' }, this.summary);
+    
+    // create  custom controls for top row
+    var customControl11 = new widjdev.Component('acitve label 11');
+    var customControl12 = new widjdev.Component('acitve label 12');
+    // create  divs for this controll
+    var  div11 = goog.dom.createDom('div', { 'class': 'customControl11' } );
+    var  div12 = goog.dom.createDom('div', { 'class': 'customControl12' } );
+    // render the controls
+    customControl11.render(div11);
+    customControl12.render(div12);
+    // add the control to the first row by adding the div we created
+    this.summaryDiv = goog.dom.createDom('div', { 'class': 'summary' }, this.summary, div11, div12 );
+
+    // create  custom controls for second row
+    var customControl21 = new widjdev.Component('acitve label 21');
+    var customControl22 = new widjdev.Component('acitve label 22');
+    // create  divs for this controll
+    var  div21 = goog.dom.createDom('div', { 'class': 'customControl21' } );
+    var  div22 = goog.dom.createDom('div', { 'class': 'customControl22' } );
+    // render the controls
+    customControl21.render(div21);
+    customControl22.render(div22);
+
+    this.contentElement = goog.dom.createDom('div', { 'class': 'description' }, this.description, div21, div22 );
+
+    this.editorElement = goog.dom.createDom('textarea', null, '');
+
+    this.editorContainer = goog.dom.createDom('div', {'style': 'display:block;'},  this.editorElement);
+
+    this.descriptionContainer = goog.dom.createDom('div', null,  this.contentElement,  this.editorContainer);
+
+    var rowDiv = goog.dom.createDom('div', { 'class': 'row' }, this.summaryDiv, this.descriptionContainer );
+		
+		//instance variable defined in Row class
+    this.parent.appendChild(rowDiv);
+    
+    // events and zippy
+    goog.events.listen(this.contentElement, goog.events.EventType.CLICK, this.openEditor, false, this);
+
+    this.zippy = new goog.ui.Zippy( this.summaryDiv, this.descriptionContainer);
+    
+    
+
+	}
 
 
 
@@ -56,4 +151,5 @@ widjdev.tabdev.setTabs= function(container) {
 
 
 goog.exportSymbol('widjdev.tabdev', widjdev.tabdev);
+goog.exportSymbol('widjdev.tabdev.tabRow', widjdev.tabdev.tabRow);
 goog.exportSymbol('widjdev.tabdev.setTabs', widjdev.tabdev.setTabs); 
